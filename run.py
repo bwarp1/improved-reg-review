4b67d4157dd8a753936f0e1831e662f5f38e19d8
bwarp1 <benjamin.warpinski@gmail.com>
2025-03-11 20:55:46 +0000

    good to go

---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Launcher script for the Regulatory Compliance Analysis PoC."""
+
+import sys
+from compliance_poc.src.main import main
+
+if __name__ == "__main__":
+    sys.exit(main())
